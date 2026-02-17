1e13e8e56576595bbd033cb3b5a8461366410d34
Adarsh-kumar12-sys <adarshpandey9910@gmail.com>
2026-02-18 01:00:59 +0530

    Fix img src for profile picture
    Updated the src attribute of the img tag for the profile picture.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -129,7 +129,7 @@ const navigate = useNavigate();
                 <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
               </svg> */}
                <img
-  src={ProfilePic}
+  src={}
   alt="Profile"
   className="object-cover w-full h-full rounded-full object-top scale-[0.98]"
 />
