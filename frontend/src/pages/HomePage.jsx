70fc2dd4a3ab2c80e8e40e2399d3f5235d1316da
Adarsh-kumar12-sys <adarshpandey9910@gmail.com>
2026-02-18 01:12:56 +0530

    Comment out image in HomePage
    Comment out the image element in HomePage component.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -128,10 +128,10 @@ const navigate = useNavigate();
               {/* <svg className="h-32 w-32 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
               </svg> */}
-               <img
+               {/* <img
   alt="Profile"
   className="object-cover w-full h-full rounded-full object-top scale-[0.98]"
-/>
+/> */}
 
             </div>
             </div>
